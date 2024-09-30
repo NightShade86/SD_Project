ff4f3e516bddb148906648c80c3e710e5b28b410
NightShade86 <85592494+NightShade86@users.noreply.github.com>
2024-09-30 19:30:53 +0800

    Update logout.php
    changed unset to destroy all by getting all current session

--- logout.php
+++ logout.php
@@ -1,21 +1,9 @@
 <?php
 
 session_start();
-
-if(isset($_SESSION['USER'])){
-    unset($_SESSION['USER']);
-}
-
-if(isset($_SESSION['LOGGED_IN'])){
-    unset($_SESSION['LOGGED_IN']);
-}
-
-header("Location: login_guess.php");
-die;
-
-//[READ ME] this code snippet is still under testing
-
-// Redirect to the login page
+$_SESSION = array();
+session_destroy();
 header("Location: login_guess.php");
 exit;
+
 ?>
