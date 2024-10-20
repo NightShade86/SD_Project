c3a8f46916abea9d8fd37fc013684847626324f8
Teha-fn6 <nurfatihahbintimohamad05@gmail.com>
2024-10-21 01:33:36 +0800

    IGNORE

--- db_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "clinicdb";
-
-// Open connection
-$connection = new mysqli($servername, $username, $password, $dbname);
-?>
